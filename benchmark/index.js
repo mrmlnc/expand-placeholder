404a875c5614ea47c1872b03a5bd1bdc481f6582
Denis Malinochkin <mrmlnc@yandex-team.ru>
2017-11-19 13:49:46 +0300

    chore(bench): Add some benchmark suites

--- benchmark/index.js
+++ benchmark/index.js
@@ -2,13 +2,13 @@
 
 const vm = require('vm');
 
-const expandPlaceholder = require('../');
 const string = require('string');
 const stringPlaceholder = require('string-placeholder');
+const expandTemplate = require('expand-template');
+const lodashTemplate = require('lodash.template');
+const underscoreTemplate = require('underscore.template');
 
-const lodash = require('lodash');
-lodash.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
-
+const expandPlaceholder = require('../');
 const vmReplace = require('./vm');
 
 const str = `
@@ -21,12 +21,12 @@ const str = `
 `;
 
 const data = {
-    a: 'a', b: 'b', c: 'c', d: 'd', e: 5,
-    f: 'f', g: 'g', h: 'h', i: 'i', j: 10,
-    k: 'k', l: 'l', m: 'm', n: 'n', o: 15,
-    p: 'p', q: 'q', r: 'r', s: 's', t: 20,
-    u: 'u', v: 'v', w: 'w', x: 'x', y: 25,
-    z: 'z'
+	a: 'a', b: 'b', c: 'c', d: 'd', e: 5,
+	f: 'f', g: 'g', h: 'h', i: 'i', j: 10,
+	k: 'k', l: 'l', m: 'm', n: 'n', o: 15,
+	p: 'p', q: 'q', r: 'r', s: 's', t: 20,
+	u: 'u', v: 'v', w: 'w', x: 'x', y: 25,
+	z: 'z'
 };
 
 const ctx = vm.createContext(data);
@@ -35,35 +35,47 @@ let res = '';
 
 suite('Expand', () => {
 
-    set('iterations', 10000);
+	set('type', 'static');
+	set('iterations', 1e6);
 
-    bench('expand-placeholder', () => {
-        res = expandPlaceholder(str, data);
-    });
+	bench('expand-placeholder', () => {
+		res = expandPlaceholder(str, data);
+	});
 
-    bench('string.template', () => {
-        res = string(str).template(data, '{{', '}}');
+	bench('string.template', () => {
+		res = string(str).template(data, '{{', '}}');
+	});
 
-    });
+	bench('expand-template', () => {
+		res = expandTemplate({ sep: '{{}}' })(str, data);
+	});
 
-    bench('string-placeholder', () => {
-        res = stringPlaceholder(str, data, {
-            before: '{{',
-            after: '}}'
-        });
-    });
+	bench('string-placeholder', () => {
+		res = stringPlaceholder(str, data, {
+			before: '{{',
+			after: '}}'
+		});
+	});
 
-    bench('lodash.template', () => {
-        // Why `lodash.template` used here?
-        // ---
-        // Most likely your string will be generated dynamically. Lodash parses the string once and
-        // does not allow to change it. So be honest make parse string every time before calling.
-        res = lodash.template(str)(data);
-    });
+	bench('lodash.template', () => {
+		// Why `lodash.template` used here?
+		// ---
+		// Most likely your string will be generated dynamically. Lodash parses the string once and
+		// does not allow to change it. So be honest make parse string every time before calling.
+		res = lodashTemplate(str, { interpolate: /{{([\s\S]+?)}}/g })(data);
+	});
 
-    bench('vm', () => {
-        // https://github.com/posthtml/posthtml-expressions/blob/d80c026536e849d82d98d9970f72d6de4d6f2d4b/lib/expression_parser.js
-        res = vmReplace(ctx, [['{{', '}}']], str);
-    });
+	bench('underscore.template', () => {
+		// Why `underscore.template` used here?
+		// ---
+		// Most likely your string will be generated dynamically. Underscore parses the string once and
+		// does not allow to change it. So be honest make parse string every time before calling.
+		res = underscoreTemplate(str, data, { interpolate: /{{([\s\S]+?)}}/g });
+	});
+
+	bench('vm', () => {
+		// https://github.com/posthtml/posthtml-expressions/blob/d80c026536e849d82d98d9970f72d6de4d6f2d4b/lib/expression_parser.js
+		res = vmReplace(ctx, [['{{', '}}']], str);
+	});
 
 });
